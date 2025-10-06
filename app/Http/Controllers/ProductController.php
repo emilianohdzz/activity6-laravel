eb20a2cd3510c9a011d14bb04a20c6039a207ce5
Emiliano . <emilianohernandez295@gmail.com>
2025-10-05 20:04:06 -0600

    homework5

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('products.index');
+    }
+
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    public function edit($id)
+    {
+        return view('products.edit');
+    }
+
+    public function store(Request $request) {}
+    public function show($id) {}
+    public function update(Request $request, $id) {}
+    public function destroy($id) {}
 }
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -5,5 +5,9 @@
 </head>
 <body>
     <h1>Vista de Productos</h1>
+    
+    <a href="{{ route('products.create') }}">
+        <button type="button">Add Product</button>
+    </a>
 </body>
 </html>
